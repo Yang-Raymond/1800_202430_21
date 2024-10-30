68a59e14c0e6f4c6a7422c8d3e8a08a64c63f74a
Ryan Chu <rchu39@my.bcit.ca>
2024-10-30 03:33:46 -0700

    Added station-info.html, welcome.html, skeleton.js, and navbar.html

---
+++ scripts/skeleton.js
@@ -0,0 +1,11 @@
+function loadSkeleton() {
+    fetch("./text/navbar.html")
+        .then(
+            response => {return response.text()}
+        )
+        .then(
+            data => {document.getElementById("navbar-placeholder").innerHTML = data }
+        );
+};
+loadSkeleton()
+console.log("Skeleton.js loaded")
