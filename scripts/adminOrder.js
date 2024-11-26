7a71aa62df16b7b2bf72ed6d091e86e9429b579a
unknown <raymondyang59@gmail.com>
2024-11-25 17:40:40 -0800

    display invoice when order dropdown is clicked

--- scripts/adminOrder.js
+++ scripts/adminOrder.js
@@ -1,15 +1,15 @@
 import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 import { firebaseConfig } from "./firebaseAPI.js";
-import { getFirestore, collection, getDocs, doc, getDoc, updateDoc, setDoc, query, where } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
+import { getFirestore, collection, getDocs, doc, getDoc, updateDoc, setDoc, query, where, onSnapshot } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 
-//Initalize Firebase authentication and Firestore
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
-//Get order template and locate the order container
 const orderTemplate = document.getElementById("orderTemplate");
 const orderContainer = document.getElementById("ordersContainer");
+const productTemplate = document.getElementById("productTemplate");
+const productContainer = document.getElementById("productContainer");
+let onTop = document.getElementsByClassName("ontop");
 
 //Logout user if logout button is clicked
 document.getElementById("logoutBtn").addEventListener("click", () => {
@@ -29,6 +29,7 @@ document.getElementById("pending").addEventListener("click", () => {
     document.getElementById("dispatchBorder").style.borderBottom = "none";
     document.getElementById("completedBorder").style.borderBottom = "none";
     document.getElementById("approvedBorder").style.borderBottom = "none";
+    document.getElementById("rejectedBorder").style.borderBottom = "none";
     getOrders("pending");
 });
 document.getElementById("approve").addEventListener("click", () => {
@@ -37,6 +38,7 @@ document.getElementById("approve").addEventListener("click", () => {
     document.getElementById("dispatchBorder").style.borderBottom = "none";
     document.getElementById("completedBorder").style.borderBottom = "none";
     document.getElementById("pendingBorder").style.borderBottom = "none";
+    document.getElementById("rejectedBorder").style.borderBottom = "none";
     getOrders("approved");
 });
 document.getElementById("dispatch").addEventListener("click", () => {
@@ -68,30 +70,96 @@ document.getElementById("rejected").addEventListener("click", () => {
 });
 
 // Grabs all orders from each station and displays it
-async function getOrders(filter){
+async function getOrders(filter) {
     orderContainer.innerHTML = "";
     const mainCollection = await getDocs(collection(db, "stations"));
     mainCollection.forEach(async (station) => {
-
         const stationFields = await doc(db, "stations", station.id);
         const stationFieldsSnap = await getDoc(stationFields);
         const brand = stationFieldsSnap.data().brand;
         const number = stationFieldsSnap.data().number;
         const address = stationFieldsSnap.data().address;
         const phone = stationFieldsSnap.data().phone;
+        const q = query(collection(db, "stations", station.id, "loads"));
+        onSnapshot(q, (querySnapshot) => {
+            orderContainer.innerHTML = "";
+            querySnapshot.forEach(async (load) => {
+                if (load.data().status == filter) {
+                    const clone = orderTemplate.content.cloneNode(true);
+                    const dropdown = clone.querySelector("#dropdown");
+                    let subtotal = 0;
+                    clone.querySelector("#station").innerText = brand + " #" + number;
+                    clone.querySelector("#invoiceNum").innerText = station.id + load.id;
+                    clone.querySelector("#stationAddress").innerText = address;
+                    clone.querySelector("#ETA").innerText = convertTimestamp(load.data().deliveryWindowFrom) + " - " + convertTimestamp(load.data().deliveryWindowTo);
+                    clone.querySelector("#price").innerText = load.data().price;
+                    clone.querySelector("#" + filter + "Badge").style.display = "block";
+                    const products = await getDocs(collection(db, "stations", station.id, "loads", load.id, "compartments"));
+                    dropdown.addEventListener("click", () => {
+                        productContainer.innerHTML = "";
 
-        const loads = await getDocs(collection(db, "stations", station.id, "loads"));
-        loads.forEach(async (load) => {
-            if (load.data().status == filter) {
-                const clone = orderTemplate.content.cloneNode(true);
-                clone.querySelector("#station").innerText = brand + " #" + number;
-                clone.querySelector("#invoiceNum").innerText = station.id + load.id;
-                clone.querySelector("#stationAddress").innerText = address;
-                clone.querySelector("#ETA").innerText = convertTimestamp(load.data().deliveryWindowFrom) + " - " + convertTimestamp(load.data().deliveryWindowTo);
-                clone.querySelector("#price").innerText = load.data().price;
-                clone.querySelector("#"+filter+"Badge").style.display="block";
-                orderContainer.appendChild(clone);
-            }
+                        //Gets each product in an order and displays it
+                        products.forEach((product) => {
+                            const cloneProduct = productTemplate.content.cloneNode(true);
+                            cloneProduct.querySelector("#compartmentNum").innerText = product.id;
+                            cloneProduct.querySelector("#product").innerText = product.data().fuelType;
+                            cloneProduct.querySelector("#quantity").innerText = product.data().fuelVolume;
+                            cloneProduct.querySelector("#unitPrice").innerText = "$1.42";
+                            cloneProduct.querySelector("#totalAmount").innerText = (1.42 * Number(product.data().fuelVolume)).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+                            subtotal += 1.42 * Number(product.data().fuelVolume);
+                            productContainer.appendChild(cloneProduct);
+                        });
+                        const orderNum = document.querySelectorAll(".orderNum");
+                        onTop[0].style.display = "block";
+                        document.getElementById("overlay").style.display = "block";
+                        document.getElementById("brand").innerText = station.data().brand;
+                        document.getElementById("number").innerText = station.data().number;
+                        document.getElementById("address").innerText = station.data().address;
+                        document.getElementById("phone").innerText = station.data().phone;
+                        document.getElementById("creationDate").innerText = convertTimestamp(load.data().dateCreated);
+                        document.getElementById("actionBtn").style.display = "none";
+                        document.getElementById("productPendingBadge").style.display = "none";
+                        document.getElementById("productApprovedBadge").style.display = "none";
+                        document.getElementById("productRejectedBadge").style.display = "none";
+                        document.getElementById("productDispatchedBadge").style.display = "none";
+                        document.getElementById("productCompletedBadge").style.display = "none";
+                        document.getElementById("comments").innerText = load.data().comments;
+                        document.getElementById("subtotal").innerText = subtotal.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+                        document.getElementById("tax").innerText = (subtotal * 0.12).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+                        document.getElementById("total").innerText = (subtotal * 1.12).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+
+                        //Updates order status if button is pressed
+                        document.getElementById("rejectBtn").addEventListener("click", async () => {
+                            const statusRef = doc(db, "stations", station.id, "loads", load.id);
+                            await updateDoc(statusRef, {
+                                status: "rejected",
+                            });
+                        });
+                        document.getElementById("approveBtn").addEventListener("click", async () => {
+                            const statusRef = doc(db, "stations", station.id, "loads", load.id);
+                            await updateDoc(statusRef, {
+                                status: "approved",
+                            });
+                        });
+                        if (load.data().status == "pending") {
+                            document.getElementById("productPendingBadge").style.display = "inline-flex";
+                            document.getElementById("actionBtn").style.display = "block";
+                        } else if (load.data().status == "approved") {
+                            document.getElementById("productApprovedBadge").style.display = "inline-flex";
+                        } else if (load.data().status == "rejected") {
+                            document.getElementById("productRejectedBadge").style.display = "inline-flex";
+                        } else if (load.data().status == "dispatched") {
+                            document.getElementById("productDispatchedBadge").style.display = "inline-flex";
+                        } else {
+                            document.getElementById("productCompletedBadge").style.display = "inline-flex";
+                        }
+                        orderNum.forEach((order) => {
+                            order.innerText = station.id + load.id;
+                        });
+                    });
+                    orderContainer.appendChild(clone);
+                }
+            });
         });
     });
 }
@@ -107,3 +175,8 @@ function convertTimestamp(timestamp) {
     date = mm + '/' + dd + '/' + yyyy;
     return date;
 }
+
+document.getElementById("closeBtn").addEventListener("click", () => {
+    onTop[0].style.display = "none";
+    document.getElementById("overlay").style.display = "none"
+});
