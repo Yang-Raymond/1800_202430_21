80e977ae2e613fe6b01f2cc67cab095ef7ef908d
Raymond Yang <raymondyang59@gmail.com>
2024-12-02 16:31:45 -0800

    fixed create station duplication bug

--- scripts/adminStation.js
+++ scripts/adminStation.js
@@ -10,6 +10,8 @@ const createStationForm = document.getElementById("createStationForm");
 const stationTemplate = document.getElementById("stationTemplate").content;
 const stationContainer = document.getElementById("stationContainer");
 const cancelBtn = document.getElementsByClassName("cancelBtn");
+const dropdownToggle = document.getElementsByClassName("dropdown-toggle");
+const dropdownMenu = document.getElementsByClassName("dropdown-menu");
 let sidebar = document.getElementsByClassName("sidebar");
 
 //Checks if user is signed in
@@ -26,10 +28,10 @@ onAuthStateChanged(auth, (user) => {
         });
 
         //Display create station form if button is clicked
-        document.getElementById("createStationAccBtn").addEventListener("click", () => {
+        document.getElementById("createStationAccBtn").onclick = () => {
             createStationForm.style.display = "block";
             document.getElementById("overlay").style.display = "block";
-        });
+        };
 
         //Close create station form or edit station form if button is clicked
         cancelBtn[0].onclick = () => {
@@ -37,12 +39,16 @@ onAuthStateChanged(auth, (user) => {
             document.getElementById("overlay").style.display = "none";
         };
         cancelBtn[1].onclick = () => {
+            document.getElementById("createStationRestrictionForm").style.display = "none";
+            document.getElementById("overlay").style.display = "none";
+        };
+        cancelBtn[2].onclick = () => {
             document.getElementById("editStationAccForm").style.display = "none";
             document.getElementById("overlay").style.display = "none";
         };
 
         //Account is created when button is clicked
-        document.getElementById("createAccBtn").onclick = () => {
+        document.getElementById("createAccBtn").onclick = async () => {
             const brandInput = document.getElementById("brandInput");
             const stationNumInput = document.getElementById("stationNumInput");
             const stationAddressInput = document.getElementById("stationAddressInput");
@@ -50,31 +56,8 @@ onAuthStateChanged(auth, (user) => {
             const emailInput = document.getElementById("emailInput");
             const passwordInput = document.getElementById("passwordInput");
             const repeatPasswordInput = document.getElementById("repeatPasswordInput");
-            if (brandInput.value && stationNumInput.value && stationAddressInput.value && stationPhoneNumInput.value && emailInput.value && passwordInput.value && passwordInput.value == repeatPasswordInput.value) {
-                createUserWithEmailAndPassword(auth, emailInput.value, passwordInput.value)
-                    .then(async (userCredential) => {
-                        const user = userCredential.user;
-                        await setDoc(doc(db, "stations", user.uid), {
-                            brand: brandInput.value,
-                            email: emailInput.value,
-                            address: stationAddressInput.value,
-                            phone: stationPhoneNumInput.value,
-                            role: "station",
-                            number: stationNumInput.value,
-                            username: brandInput.value + "#" + stationNumInput.value
-                        });
-                        document.getElementById("sucessAlert").style.display = "block";
-                        setTimeout(() => {
-                            brandInput.value = "";
-                            stationNumInput.value = "";
-                            stationAddressInput.value = "";
-                            stationPhoneNumInput.value = "";
-                            emailInput.value = "";
-                            passwordInput.value = "";
-                            repeatPasswordInput.value = "";
-                            document.getElementById("sucessAlert").style.display = "none";
-                        }, 1500)
-                    })
+            if (brandInput.value && stationNumInput.value && stationAddressInput.value && stationPhoneNumInput.value && emailInput.value && passwordInput.value && passwordInput.value == repeatPasswordInput.value && !isNaN(stationNumInput.value)) {
+                const userCredential = await createUserWithEmailAndPassword(auth, emailInput.value, passwordInput.value)
                     .catch((error) => {
                         const errorCode = error.code;
                         const errorMessage = error.message;
@@ -87,6 +70,28 @@ onAuthStateChanged(auth, (user) => {
                             document.getElementById("errorAlert").style.display = "none";
                         }, 1500)
                     });
+                setDoc(doc(db, "stations", userCredential.user.uid), {
+                    brand: brandInput.value,
+                    email: emailInput.value,
+                    address: stationAddressInput.value,
+                    phone: stationPhoneNumInput.value,
+                    role: "station",
+                    number: stationNumInput.value,
+                    username: brandInput.value + "#" + stationNumInput.value
+                });
+                document.getElementById("sucessAlert").style.display = "block";
+                setTimeout(() => {
+                    brandInput.value = "";
+                    stationNumInput.value = "";
+                    stationAddressInput.value = "";
+                    stationPhoneNumInput.value = "";
+                    emailInput.value = "";
+                    passwordInput.value = "";
+                    repeatPasswordInput.value = "";
+                    document.getElementById("sucessAlert").style.display = "none";
+                    document.getElementById("createStationForm").style.display = "none";
+                    document.getElementById("overlay").style.display="none";
+                }, 1000);
             } else {
                 document.getElementById("errorAlert").style.display = "block";
                 setTimeout(() => {
@@ -94,55 +99,6 @@ onAuthStateChanged(auth, (user) => {
                 }, 1500)
             }
         }
-
-        //Gets all stations and displays them
-        const q = query(collection(db, "stations"));
-        onSnapshot(q, (querySnapshot) => {
-            stationContainer.innerHTML = "";
-            querySnapshot.forEach(async (station) => {
-                if (station.data().role != "Admin") {
-                    const clone = stationTemplate.cloneNode(true);
-                    const stationFields = await doc(db, "stations", station.id);
-                    const stationFieldsSnap = await getDoc(stationFields);
-                    const brand = stationFieldsSnap.data().brand;
-                    const number = stationFieldsSnap.data().number;
-                    const address = stationFieldsSnap.data().address;
-                    const email = stationFieldsSnap.data().email;
-                    clone.querySelector("#brand").innerText = brand;
-                    clone.querySelector("#stationNum").innerText = number;
-                    clone.querySelector("#email").innerText = email;
-                    clone.querySelector("#address").innerText = address;
-                    clone.querySelector("button").onclick = () => {
-                        document.getElementById("formTitle").innerText = brand + " #" + number;
-                        document.getElementById("stationID").innerText = station.id;
-                        document.getElementById("editStationAccForm").style.display = "block";
-                        document.getElementById("overlay").style.display = "block";
-                        document.getElementById("brandUpdate").value = brand;
-                        document.getElementById("stationNumUpdate").value = number;
-                        document.getElementById("stationAddressUpdate").value = address;
-                        document.getElementById("stationPhoneNumUpdate").value = stationFieldsSnap.data().phone;
-                        document.getElementById("emailUpdate").value = email;
-
-                        //If update button is clicked, station details will update
-                        document.getElementById("updateBtn").onclick = async () => {
-                            await updateDoc(doc(db, "stations", station.id), {
-                                brand: document.getElementById("brandUpdate").value,
-                                number: document.getElementById("stationNumUpdate").value,
-                                address: document.getElementById("stationAddressUpdate").value,
-                                phone: document.getElementById("stationPhoneNumUpdate").value,
-                                email: document.getElementById("emailUpdate").value
-
-                            });
-                            document.getElementById("updateSucessAlert").style.display = "block";
-                            setTimeout(() => {
-                                document.getElementById("updateSucessAlert").style.display = "none";
-                            }, 1500);
-                        };
-                    }
-                    stationContainer.appendChild(clone);
-                }
-            });
-        });
 
         //If on desktop, sidebar displays. If on mobile, sidebar hides.
         window.addEventListener("resize", () => {
@@ -166,3 +122,68 @@ onAuthStateChanged(auth, (user) => {
         console.log("User not logged in");
     }
 });
+
+//Gets all stations and displays them
+const q = query(collection(db, "stations"));
+onSnapshot(q, (querySnapshot) => {
+    stationContainer.innerHTML = "";
+    querySnapshot.forEach(async (station) => {
+        if (station.data().role != "Admin") {
+            const clone = stationTemplate.cloneNode(true);
+            const stationFields = await doc(db, "stations", station.id);
+            const stationFieldsSnap = await getDoc(stationFields);
+            const brand = stationFieldsSnap.data().brand;
+            const number = stationFieldsSnap.data().number;
+            const address = stationFieldsSnap.data().address;
+            const email = stationFieldsSnap.data().email;
+            clone.querySelector("#brand").innerText = brand;
+            clone.querySelector("#stationNum").innerText = number;
+            clone.querySelector("#email").innerText = email;
+            clone.querySelector("#address").innerText = address;
+            clone.querySelector("button").onclick = () => {
+                document.getElementById("formTitle").innerText = brand + " #" + number;
+                document.getElementById("stationID").innerText = station.id;
+                document.getElementById("editStationAccForm").style.display = "block";
+                document.getElementById("overlay").style.display = "block";
+                document.getElementById("brandUpdate").value = brand;
+                document.getElementById("stationNumUpdate").value = number;
+                document.getElementById("stationAddressUpdate").value = address;
+                document.getElementById("stationPhoneNumUpdate").value = stationFieldsSnap.data().phone;
+                document.getElementById("emailUpdate").value = email;
+
+                //If update button is clicked, station details will update
+                document.getElementById("updateBtn").onclick = async () => {
+                    await updateDoc(doc(db, "stations", station.id), {
+                        brand: document.getElementById("brandUpdate").value,
+                        number: document.getElementById("stationNumUpdate").value,
+                        address: document.getElementById("stationAddressUpdate").value,
+                        phone: document.getElementById("stationPhoneNumUpdate").value,
+                        email: document.getElementById("emailUpdate").value
+
+                    });
+                    document.getElementById("updateSucessAlert").style.display = "block";
+                    setTimeout(() => {
+                        document.getElementById("updateSucessAlert").style.display = "none";
+                    }, 1500);
+                };
+            }
+            stationContainer.appendChild(clone);
+        }
+    });
+});
+
+dropdownToggle[0].onclick = () => {
+    if (dropdownMenu[0].style.display == "block") {
+        dropdownMenu[0].style.display = "none";
+    } else {
+        dropdownMenu[0].style.display = "block";
+    }
+}
+
+dropdownToggle[1].onclick = () => {
+    if (dropdownMenu[1].style.display == "block") {
+        dropdownMenu[1].style.display = "none";
+    } else {
+        dropdownMenu[1].style.display = "block";
+    }
+}
